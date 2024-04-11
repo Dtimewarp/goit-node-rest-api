9f1b6df65082cf32a201804b01dc4454073acdf2
Dtimewarp <dtimewarp@gmail.com>
2024-04-11 15:39:09 +0300

    logout - OK

--- controllers/authControllers.js
+++ controllers/authControllers.js
@@ -37,9 +37,9 @@ export const registerUser = async (req, res) => {
     }
   };
 
-export const loginUser = async (req, res) => {
-  const {SECRET_KEY} = process.env;
-
+  export const loginUser = async (req, res) => {
+    const { SECRET_KEY } = process.env;
+  
     const { error } = loginSchema.validate(req.body);
     if (error) {
       return res.status(400).json({ message: error.details[0].message });
@@ -48,7 +48,6 @@ export const loginUser = async (req, res) => {
     const { email, password } = req.body;
   
     try {
-      
       const user = await User.findOne({ email });
   
       if (!user) {
@@ -62,36 +61,35 @@ export const loginUser = async (req, res) => {
   
       const token = jwt.sign({ userId: user._id }, SECRET_KEY, { expiresIn: '12h' });
   
+      // Збереження токену в користувача в базі даних
+      user.token = token;
+      await user.save();
+  
       res.status(200).json({ token, user: { email: user.email, subscription: user.subscription } });
     } catch (error) {
       console.error(error);
       res.status(500).json({ message: 'Server Error' });
     }
-};
+  };
   
 export const logoutUser = async (req, res, next) => {
   try {
-    // Перевірка, чи існує користувач в req.user
+  
     if (!req.user) {
       return res.status(401).json({ message: 'Not authorized-1' });
     }
 
-    // Отримання id користувача з об'єкта req
     const userId = req.user._id;
 
-    // Пошук користувача за id
     const user = await User.findById(userId);
 
-    // Перевірка наявності користувача
     if (!user) {
       return res.status(401).json({ message: 'Not authorized-2' });
     }
 
-    // Видалення токена у користувача
     user.token = null;
     await user.save();
 
-    // Відправка успішної відповіді
     return res.status(204).end();
   } catch (error) {
     console.error(error);
