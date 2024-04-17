104a03640c40fa9b7a334701384bbeaa3854f7d2
Dtimewarp <dtimewarp@gmail.com>
2024-04-17 13:20:37 +0300

    add jsArchive

--- controllers/contactsControllers.js
+++ controllers/contactsControllers.js
@@ -1,8 +1,6 @@
 
-import { listContacts, getContactById, removeContact, addContact, updateContactById, updateStatusContact  } from "../services/contactsServices.js";
+import {  getContactById, removeContact, addContact, updateContactById, updateStatusContact  } from "../services/contactsServices.js";
 import { createContactSchema, updateContactSchema, validateUpdateStatus  } from "../schemas/contactsSchemas.js";
-// import { isValidId } from "../helpers/idValidation.js";
-// import { Types } from 'mongoose';
 import { Contact } from "../db/contactModel.js";
 
 
@@ -171,40 +169,6 @@ export const createContact = async (req, res, next) => {
 };
 
 // PUT
-// export const updateContact = async (req, res) => {
-//     const { id } = req.params;
-//     const { body } = req;
-//     const ownerId = req.user._id;
-
-//     try {
-//         if (Object.keys(body).length === 0) {
-//             return res.status(400).json({ message: 'Body must have at least one field' });
-//         }
-
-//         const validation = updateContactSchema.validate(body, { abortEarly: false });
-//         if (validation.error) {
-//             return res.status(400).json({ message: validation.error.message });
-//         }
-
-//         isValidId(req, res, async () => {
-//             try {
-//                 const existingContact = await Contact.findOne({ _id: id, owner: ownerId });
-//                 if (!existingContact) {
-//                     return res.status(404).json({ message: 'Contact not found' });
-//                 }
-
-//                 const updatedContact = await updateContactById(id, body);
-//                 return res.status(200).json(updatedContact);
-//             } catch (error) {
-//                 console.error(error);
-//                 return res.status(500).json({ message: 'Server error' });
-//             }
-//         });
-//     } catch (error) {
-//         console.error(error);
-//         return res.status(500).json({ message: 'Server error' });
-//     }
-// };
 
 export const updateContact = async (req, res) => {
     const { id } = req.params;
@@ -241,40 +205,6 @@ export const updateContact = async (req, res) => {
 
 
 //PATCH
-// export const updateContactStatus = async (req, res) => {
-//     const { contactId } = req.params;
-//     const { error: validationError } = validateUpdateStatus(req.body);
-
-//     if (validationError) {
-//         return res.status(400).json({ message: validationError.details[0].message });
-//     }
-
-//     if (!Types.ObjectId.isValid(contactId)) {
-//         return res.status(400).json({ error: `${contactId} is not a valid ObjectId` });
-//     }
-
-//     const { favorite } = req.body;
-
-//     try {
-//         const contact = await Contact.findById(contactId);
-
-//         if (!contact) {
-//             return res.status(404).json({ message: "Contact not found" });
-//         }
-
-//         if (contact.owner.toString() !== req.user._id.toString()) {
-//             return res.status(403).json({ message: "You are not authorized to update this contact" });
-//         }
-
-//         contact.favorite = favorite;
-//         await contact.save();
-
-//         res.status(200).json(contact);
-//     } catch (error) {
-//         console.error(error);
-//         res.status(500).json({ message: "Server Error" });
-//     }
-// };
 
 export const updateContactStatus = async (req, res) => {
     const { id } = req.params;
--- services/contactsServices.js
+++ services/contactsServices.js
@@ -12,16 +12,6 @@ export async function listContacts() {
     }
 }
 
-// export async function getContactById(contactId) {
-//     try {
-//         const contacts = await listContacts();
-//         const contact = contacts.find(({ id }) => id === contactId);
-//         return contact || null;
-//     } catch (error) {
-//         console.log(error);
-//     }
-// }
-
 export async function getContactById(contactId) {
     try {
         const contact = await Contact.findById(contactId);
@@ -31,16 +21,6 @@ export async function getContactById(contactId) {
     }
 }
 
-// export async function removeContact(contactId) {
-//     try {
-//         const removedContact = await Contact.findByIdAndDelete(contactId);
-//         if (!removedContact) return null;
-        
-//         return removedContact;
-//     } catch (error) {
-//         console.log(error);
-//     }
-// }
 
 export async function removeContact(contactId, ownerId) {
     try {
@@ -62,18 +42,6 @@ export async function addContact(name, email, phone, ownerId) {
     }
 }
 
-// export async function updateContactById(id, newData) {
-//     try {
-//         const updatedContact = await Contact.findByIdAndUpdate(
-//             id,
-//             newData,
-//             { new: true } 
-//         );
-//         return updatedContact;
-//     } catch (error) {
-//         throw new Error('Failed to update contact');
-//     }
-// }
 
 export async function updateContactById(id, ownerId, newData) {
     try {
@@ -88,20 +56,6 @@ export async function updateContactById(id, ownerId, newData) {
         throw new Error('Failed to update contact');
     }
 }
-
-// export async function updateStatusContact(contactId, newData) {
-//     try {
-//         const updatedContact = await Contact.findByIdAndUpdate(
-//             contactId,
-//             { $set: newData },
-//             { new: true }
-//         );
-//         return updatedContact;
-//     } catch (error) {
-//         console.error(error);
-//         throw new Error("Failed to update contact status");
-//     }
-// }
 
 export async function updateStatusContact(contactId, newData) {
     try {
