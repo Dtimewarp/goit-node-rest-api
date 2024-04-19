d1180e686cdc2467d3a7584625129be18c923ade
Dtimewarp <dtimewarp@gmail.com>
2024-04-19 20:45:23 +0300

    change userService

--- services/usersService.js
+++ services/usersService.js
@@ -5,51 +5,51 @@ import fs from 'fs/promises';
 
 import { User } from '../db/userModel.js';
 
-// export const processAvatar = async (avatarFile) => {
-//     try {
-//
-//         const image = await jimp.read(avatarFile.path);
-//         await image.resize(250, 250).writeAsync(avatarFile.path);
+export const processAvatar = async (avatarFile) => {
+    try {
 
-//         const fileName = `${uuidv4()}${path.extname(avatarFile.originalname)}`;
+        const image = await jimp.read(avatarFile.path);
+        await image.resize(250, 250).writeAsync(avatarFile.path);
 
-//         await fs.rename(avatarFile.path, `public/avatars/${fileName}`);
+        const fileName = `${uuidv4()}${path.extname(avatarFile.originalname)}`;
 
-//         const avatarURL = `/avatars/${fileName}`;
-//         return avatarURL;
-//     } catch (error) {
-//         throw new Error("Failed to process avatar");
-//     }
-// };
+        await fs.rename(avatarFile.path, `public/avatars/${fileName}`);
 
-export const processAvatar = async (avatarFile, userId) => {
-	try {
-		const user = await User.findById(userId);
-
-		if (!user) {
-			throw new Error('User not found');
-		}
-
-		console.log('User ID:', user._id);
-
-		const image = await jimp.read(avatarFile.path);
-		await image.resize(250, 250).writeAsync(avatarFile.path);
-
-		const fileName = `${uuidv4()}${path.extname(avatarFile.originalname)}`;
-
-		const userAvatarFolderPath = `public/avatars/${userId}`;
-		try {
-			await fs.access(userAvatarFolderPath);
-		} catch (error) {
-			await fs.mkdir(userAvatarFolderPath, { recursive: true });
-		}
-
-		await fs.rename(avatarFile.path, `${userAvatarFolderPath}/${fileName}`);
-
-		const avatarURL = `/avatars/${userId}/${fileName}`;
-		return avatarURL;
-	} catch (error) {
-		console.error(error);
-		throw new Error('Failed to process avatar');
-	}
+        const avatarURL = `/avatars/${fileName}`;
+        return avatarURL;
+    } catch (error) {
+        throw new Error("Failed to process avatar");
+    }
 };
+
+// export const processAvatar = async (avatarFile, userId) => {
+// 	try {
+// 		const user = await User.findById(userId);
+
+// 		if (!user) {
+// 			throw new Error('User not found');
+// 		}
+
+// 		console.log('User ID:', user._id);
+
+// 		const image = await jimp.read(avatarFile.path);
+// 		await image.resize(250, 250).writeAsync(avatarFile.path);
+
+// 		const fileName = `${uuidv4()}${path.extname(avatarFile.originalname)}`;
+
+// 		const userAvatarFolderPath = `public/avatars/${userId}`;
+// 		try {
+// 			await fs.access(userAvatarFolderPath);
+// 		} catch (error) {
+// 			await fs.mkdir(userAvatarFolderPath, { recursive: true });
+// 		}
+
+// 		await fs.rename(avatarFile.path, `${userAvatarFolderPath}/${fileName}`);
+
+// 		const avatarURL = `/avatars/${userId}/${fileName}`;
+// 		return avatarURL;
+// 	} catch (error) {
+// 		console.error(error);
+// 		throw new Error('Failed to process avatar');
+// 	}
+// };
